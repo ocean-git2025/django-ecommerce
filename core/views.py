6e5a232114b55c8e94d895c6c6070a2bff1c8bb8
Nan Jia <oceantraveler@163.com>
2026-02-27 15:45:06 +0800

    glm5模型：增加商品分类筛选功能

--- core/views.py
+++ core/views.py
@@ -350,6 +350,18 @@ class HomeView(ListView):
     paginate_by = 10
     template_name = "home.html"
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category = self.request.GET.get('category')
+        if category and category != 'all':
+            queryset = queryset.filter(category=category)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_category'] = self.request.GET.get('category', 'all')
+        return context
+
 
 class OrderSummaryView(LoginRequiredMixin, View):
     def get(self, *args, **kwargs):
